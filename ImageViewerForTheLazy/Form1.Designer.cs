447a315f39ea770a3ff8470eb592e8f4816dde0a
DoG-peer <dogeometry50@gmail.com>
2014-02-15 03:15:39 +0900

    release

---
+++ ImageViewerForTheLazy/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ImageViewerForTheLazy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.終了ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.スクリーンショットToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.終了ToolStripMenuItem,
+            this.スクリーンショットToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(185, 48);
+            // 
+            // 終了ToolStripMenuItem
+            // 
+            this.終了ToolStripMenuItem.Name = "終了ToolStripMenuItem";
+            this.終了ToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.終了ToolStripMenuItem.Text = "終了";
+            this.終了ToolStripMenuItem.Click += new System.EventHandler(this.終了ToolStripMenuItem_Click);
+            // 
+            // スクリーンショットToolStripMenuItem
+            // 
+            this.スクリーンショットToolStripMenuItem.Name = "スクリーンショットToolStripMenuItem";
+            this.スクリーンショットToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.スクリーンショットToolStripMenuItem.Text = "スクリーンショット";
+            this.スクリーンショットToolStripMenuItem.Click += new System.EventHandler(this.スクリーンショットToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "怠";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(284, 284);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.TopMost = true;
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 終了ToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem スクリーンショットToolStripMenuItem;
+    }
+}
+
---
+++ ImageViewerForTheLazy/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace ImageViewerForTheLazy
+{
+    public partial class Form1 : Form
+    {
+        int picShowFlag = 0;
+        int x,y;
+        int screen_x = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+        int screen_y = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+        string[] imgs;
+        int imgId;
+        System.Random r = new System.Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            this.WindowState = FormWindowState.Normal;
+            
+        }
+
+        private void 終了ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false; // アイコンをトレイから取り除く
+            Application.Exit(); // アプリケーションの終了
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (picShowFlag == 1 )
+            {
+                imgs = System.IO.Directory.GetFiles("img");
+                imgId = r.Next(imgs.Length);
+                pictureBox1.Image = Image.FromFile(imgs[imgId]);
+                x = pictureBox1.Width;
+                y = pictureBox1.Height;
+                this.Location = new Point(screen_x - x, screen_y - y - 40);
+                this.Visible = true;
+                picShowFlag = 0;
+            }
+            else
+            {
+                if (pictureBox1.Image != null)
+                {
+                    pictureBox1.Image.Dispose();
+                }
+                if (this.Visible)
+                {
+                    this.Visible = false;
+                }
+                picShowFlag++;
+            }
+        }
+
+       
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true;
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            //this.Activate();
+        }
+
+        private void スクリーンショットToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            string softScreen = ConfigurationManager.AppSettings["ScreenShot"];
+            System.Diagnostics.Process.Start(softScreen);//SnippingTool
+            //System.Diagnostics.Process.Start("SnippingTool.exe");//SnippingTool
+            //System.Diagnostics.Process.Start("rapture.exe");//SnippingTool
+        }
+    }
+}
